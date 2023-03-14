5b1078efcaa67fdebfce87b8ec73d1d3402a871b
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-03-14 16:40:34 +1100

    connecting all HTML elements to script

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+
+//HTML document links 
+const quizDocument = document.getElementById('#quiz')
+const startBtn = document.getElementById('#startbtn')
+const questions = document.getElementById('#questions')
+const answers = document.getElementById('#answers')
+const timer = document.getElementById('#timer')
+const score = document.getElementById('#score')
+
+
