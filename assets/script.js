e9ebe081c64ce58e08b0e2fd7af66883be7b454f
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-03-14 17:06:09 +1100

    Start button calls for timer to start, hides

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,29 @@
 
 //HTML document links 
-const quizDocument = document.getElementById('#quiz')
-const startBtn = document.getElementById('#startbtn')
-const questions = document.getElementById('#questions')
-const answers = document.getElementById('#answers')
-const timer = document.getElementById('#timer')
-const score = document.getElementById('#score')
+const quizDocument = document.getElementById('quiz')
+const startBtn = document.getElementById('startbtn')
+const questions = document.getElementById('questions')
+const answers = document.getElementById('answers')
+const timer = document.getElementById('timer')
+const score = document.getElementById('score')
+
+//Timer default count 
+let secondsLeft = 10;
 
 
+startBtn.addEventListener('click', function() {
+   //start button, prevents refresh, hides button
+    startBtn.preventDefault();
+    startBtn.hidden = true;
+    //calls for timer to start
+    timer()
+
+
+});
+
+function timer(){
+secondsLeft --;
+
+
+
+}
